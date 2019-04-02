fed212ae72fcab465fadbed98868ed9130930786
Dawid <michalskimlm@gmail.com>
2019-04-02 08:43:13 +0200

    Change pass to db

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var pool = mysql.createPool({
   connectionLimit: 10,
   host: "127.0.0.1",
   user: "root",
-  password: "admin",
+  password: "admin2",
   database: "vas_productivity_database"
 });
 
